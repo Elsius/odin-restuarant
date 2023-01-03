7b834d7465f1380273fa945fba7f04b0fc5a1043
Elsius <underseabehemoth@gmail.com>
2023-01-02 16:44:44 -0800

    initial css + loader, test objects

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-function component(){
-    let element = document.createElement('div');
+function component(element, id) {
+    let elementObject = document.createElement(element);
 
-
-    element.innerText = 'test'
-
-    return element
+    if (id != undefined) {
+        elementObject.setAttribute('id', id)
+    }
+    return elementObject
 }
-
-document.body.appendChild(component())
+import './style.css';
+//psuedo
+//init, imports original page, header, main content etc
+//add event listener to header buttons that clear the page then imports modules that contain that button's content
+//import that page's contents after clearing
+const mainContent = component('div', 'content'),
+    header = component('header'),
+    footer = component('footer');
+document.body.appendChild(header)
+document.body.appendChild(mainContent)
+document.body.appendChild(footer)
+header.innerText = 'header'
+mainContent.innerText = 'content'
+footer.innerText = 'footer'
