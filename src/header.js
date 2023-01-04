7b43cb59ce52cb5b5584dad0a365c125bd5575ce
Elsius <underseabehemoth@gmail.com>
2023-01-03 18:48:43 -0800

    header buttons logo and css

--- src/header.js
+++ src/header.js
@@ -1,17 +1,26 @@
 import { component } from './elements_Module.js';
 export function header() {
     const headerDiv = component('header'),
-        home = component('div', 'home'),
-        menu = component('div', 'menu', ['hidden']),
-        contact = component('div', 'contact', ['hidden']),
-        childElements = [home, menu, contact];
+        home = component('button', 'home'),
+        menu = component('button', 'menu', ['hidden']),
+        contact = component('button', 'contact', ['hidden']),
+        childElements = [home, menu, contact],
+        headerButtonBox = component('div'),
+        bannerLogo = component('img');
 
+    //banner
+    bannerLogo.src = "test"
+    //buttons
     childElements[0].innerText = 'Home';
     childElements[1].innerText = 'Menu';
     childElements[2].innerText = 'Contact';
 
     childElements.forEach(child => {
-        headerDiv.appendChild(child);
+        //maybe remove the event listener and handle it after content is imported to index, or have it call a function that will change up the pages
+        child.addEventListener('click', function (event) { this.classList.toggle('hidden') })
+        headerButtonBox.appendChild(child);
     });
+    headerDiv.appendChild(bannerLogo)
+    headerDiv.appendChild(headerButtonBox)
     return headerDiv
 }
